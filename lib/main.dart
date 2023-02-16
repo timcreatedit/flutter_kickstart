e126bd05fd0e2510c2c8c9ebc256be7d7e949350
Tim <hi@timcreated.it>
2023-02-16 21:02:33 +0100

    slight improvements

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        useMaterial3: true,
       ),
       routerConfig: router,
       localizationsDelegates: L10n.localizationsDelegates,
